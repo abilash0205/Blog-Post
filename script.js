afb01f66799ae9aa6086e9b7a3e1a2c6fd387aeb
Abilash <abilashraman2@gmail.com>
2023-01-14 12:48:52 +0530

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const createBlogCard = (title, date, description) => {
+const createBlogCard = (title, date, description, image) => {
     return `
-          <div class="card col" style="font-size: 12px; border: none;"> 
+          <div class="card col" style="font-size: 12px; border: none; background: #dec0d2; text-align: justify;"> 
               <p class="text-sm" style="color: rgb(128, 128, 128);">${date.toGMTString()}</p>
               <h3 style="font-size: 16px;">${title}</h3>
               <p>${description}</p>
+              <img src="${image}" alt="Post Image" id="image-link">
   
               <a class="text-primary" href="" style="text-decoration: none;">Read More</a>
+
+              <div class="alert alert-success alert-dismissible" role="alert">
+              Post Added Successfully!
+              <button class="btn-close" data-bs-dismiss="alert"></button>
+          </div>
           </div>
       `;
   };
@@ -13,7 +19,7 @@ const createBlogCard = (title, date, description) => {
   const posts = JSON.parse(localStorage.getItem("posts"));
   
   posts.forEach((post) => {
-    const postHtml = createBlogCard(post.title, new Date(), post.description);
+    const postHtml = createBlogCard(post.title, new Date(), post.description, post.image);
     const postsContainer = document.getElementById("blog-tile-container");
     const postDiv = document.createElement("div");
     postDiv.innerHTML = postHtml;
